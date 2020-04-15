ed971a2a02d72d7a0d202f84ff93a24b978e1768
Wagner Rodrigues <wagner@cognosbr.com.br>
2020-04-15 13:45:06 -0300

    Acerto no pom.xml

--- src/test/java/br/ce/wcaquino/functional/TaskTest.java
+++ src/test/java/br/ce/wcaquino/functional/TaskTest.java
@@ -17,12 +17,15 @@ public class TaskTest {
 		System.setProperty("webdriver.chrome.driver", "/home/wagner/work/programas/chromedriver_linux64/chromedriver80");
 		
 		ChromeOptions options = new ChromeOptions();
+
 		options.addArguments("start-maximized"); // open Browser in maximized mode
 		options.addArguments("disable-infobars"); // disabling infobars
 		options.addArguments("--disable-extensions"); // disabling extensions
 		options.addArguments("--disable-gpu"); // applicable to windows os only
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 		options.addArguments("--no-sandbox"); // Bypass OS security model
+		options.addArguments("--headless");
+		
 		WebDriver driver = new ChromeDriver(options);
 		
 		
