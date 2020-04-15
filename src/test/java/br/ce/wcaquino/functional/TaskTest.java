23f28a5ff9a8d82115f5810d71d9434a77816be4
Wagner Rodrigues <wagner@cognosbr.com.br>
2020-04-15 13:47:06 -0300

    Acerto no pom.xml

--- src/test/java/br/ce/wcaquino/functional/TaskTest.java
+++ src/test/java/br/ce/wcaquino/functional/TaskTest.java
@@ -18,12 +18,12 @@ public class TaskTest {
 		
 		ChromeOptions options = new ChromeOptions();
 
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
+//		options.addArguments("start-maximized"); // open Browser in maximized mode
+//		options.addArguments("disable-infobars"); // disabling infobars
+//		options.addArguments("--disable-extensions"); // disabling extensions
+//		options.addArguments("--disable-gpu"); // applicable to windows os only
+//		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//		options.addArguments("--no-sandbox"); // Bypass OS security model
 		options.addArguments("--headless");
 		
 		WebDriver driver = new ChromeDriver(options);
