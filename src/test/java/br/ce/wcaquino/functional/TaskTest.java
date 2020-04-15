789191d29faf955c7fbb7ec6a58964318855cf68
Wagner Rodrigues <wagner@cognosbr.com.br>
2020-04-15 14:00:19 -0300

    Acerto no pom.xml

--- src/test/java/br/ce/wcaquino/functional/TaskTest.java
+++ src/test/java/br/ce/wcaquino/functional/TaskTest.java
@@ -24,14 +24,11 @@ public class TaskTest {
 //		options.addArguments("--disable-gpu"); // applicable to windows os only
 //		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 //		options.addArguments("--no-sandbox"); // Bypass OS security model
+
 		options.addArguments("--headless");
 		
 		WebDriver driver = new ChromeDriver(options);
 		
-		
-		
-//		WebDriver driver = new ChromeDriver();
-
 		driver.navigate().to("http://localhost:8888/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
