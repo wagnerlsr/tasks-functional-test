7656a24493b83ee4c16c5bad1d63df7c830c9946
Wagner Rodrigues <wagner@cognosbr.com.br>
2020-04-17 00:48:31 -0300

    teste de remoção

--- src/test/java/br/ce/wcaquino/functional/TaskTest.java
+++ src/test/java/br/ce/wcaquino/functional/TaskTest.java
@@ -135,4 +135,32 @@ public class TaskTest {
 		
 	}
 	
+	@Test
+	public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+
+		WebDriver driver = acessarAplicacao();
+		
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2010");
+			driver.findElement(By.id("saveButton")).click();
+			
+			String message = driver.findElement(By.id("message")).getText();
+			
+			Assert.assertEquals("Success!", message);
+			
+			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+			
+			message = driver.findElement(By.id("message")).getText();
+			
+			Assert.assertEquals("Success!", message);
+			
+			
+		} finally {
+			driver.quit();
+		}
+		
+	}
+	
 }
